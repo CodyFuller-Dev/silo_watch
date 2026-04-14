7d27b41e751ee28298631c5e9bc934be7b4a57d0
Cody Fuller <cody.fuller.dev@gmail.com>
2026-04-14 10:30:46 -0500

    updated the notifi

--- notification_service.py
+++ notification_service.py
@@ -1,6 +1,27 @@
 #this deals with sending the email alert to the operator
 
-def send_alert(alert_msg):
-    print("[!]---------------------------------[!]")
-    print(f"Email Alert:\n {alert_msg}")
-    print("[!]---------------------------------[!]")
+#smtplib = anything email related
+#ssl = privacy layer
+#os= mac os system layer
+#.env=secrets file
+
+import smtplib
+import ssl
+import os
+from dotenv import load_dotenv
+
+#tells python to go into the file in the macos op sys
+load_dotenv()
+
+
+def send_notification(current_humidity):
+#this is the block to start an email. Here I am using the same TO: and FROM: but usually these would be different
+    sender_email = os.getenv("MY_GMAIL")
+    receiver_email = os.getenv("MY_GMAIL")
+    password = os.getenv("GMAIL_PASS")
+
+
+#laying out the structure of the email 
+    subject = "North Silo Alert [!] HIGH HUMIDITY DETECTED"
+    body = f"Warning: North Grain Silo humidity is {current_humidity}% \n Turn on Bin Dryer NOW"
+    message = f"Subject: {subject}\n\n{body}"
