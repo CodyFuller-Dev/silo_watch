606552c7e1f01c17039d7c9911033e1217f09da5
Cody Fuller <cody.fuller.dev@gmail.com>
2026-04-14 06:07:14 -0500

    Updated to email from twilio

--- notification_service.py
+++ notification_service.py
@@ -1,4 +1,6 @@
+#this deals with sending the email alert to the operator
+
 def send_alert(alert_msg):
     print("[!]---------------------------------[!]")
-    print(f"Future Text Message Alerting:\n {alert_msg}")
+    print(f"Email Alert:\n {alert_msg}")
     print("[!]---------------------------------[!]")
