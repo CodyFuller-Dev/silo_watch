553f0663c8bdaf54c9b69990f305f3ba79602339
Cody Fuller <cody.fuller.dev@gmail.com>
2026-07-28 07:11:23 -0500

    added HTML dashboard to main

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import logging
 import sys
 import os
-
-# tells the application to bring in the flask component as well as the jsonify component
-from flask import Flask, jsonify, redirect, url_for
+# tells the application to bring in the flask component, jsonify, and the NEW render_template
+from flask import Flask, jsonify, render_template
 
 # tells the program to go get the send alert called out in the if statements 
 from notification_service import send_notification
@@ -11,11 +10,10 @@ from notification_service import send_notification
 # tells the script where in the project to look for the variable needed
 from weather_service import take_humidity
 
-# tells flask where to look for the website files. Use __name__ only to keep from confusing things. It automatically stores the name of the current file aka app.py
+# tells flask where to look for the website files.
 app = Flask(__name__)
 
-# tells the application to disregard the default way of sorting. This allows for my json to be displayed on the webpage as I see fit. 
-# note this is called out as potentially problematic in some of the documents. If I encounter some bugs here would be a good place to start.
+# tells the application to disregard the default way of sorting.
 app.json.sort_keys = False
 
 # starts all the logging information, gives a timestamp I can read what the level is and message as well as creates a log file
@@ -31,24 +29,26 @@ logger = logging.getLogger(__name__)
 
 logger.info("========================================================")
 logger.info(" Silo Watch Program Initialized ")
-logger.info(" Navigate to http://127.0.0.1:5001/silo to view status ")
 logger.info("=======================================================")
 
-# this shoots the user right to the correct URL with no guesswork
+# ==========================================================================
+# ROUTE 1: Serves the HTML Dashboard on the main URL
+# ==========================================================================
 @app.route('/')
-def index():
-    return redirect(url_for('city_wrapper'))
+def home():
+    # This looks for index.html inside your new 'templates' folder
+    return render_template('index.html')
 
-# this defines what goes at the end of self hosted url to return what we are looking for
-@app.route('/silo')
-# struggled with this, def XX I am just wrapping the variable "city" in a define so I can call it up later. 
+# ==========================================================================
+# ROUTE 2: Provides the JSON data for the dashboard's JavaScript
+# ==========================================================================
+@app.route('/silo-humidity')
 def city_wrapper():
     city = "Charleston,IL,US"
-
     # current_humidity is just a placeholder name so that we can call take_humidity from the weather service mod and pass the "city" to it
     current_humidity = take_humidity(city)
     
-    # when the API is called up and no data is returned for whatever reason an error message is sent
+    # when the API is called up and no data is returned for what ever reason an error message is sent
     if current_humidity is None:
         error_msg = "CRITICAL: Sensor Offline. Manual Data Reading Required!"
         logger.error(f"Weather API failure: {error_msg}")
@@ -64,14 +64,16 @@ def city_wrapper():
     else:
         logger.info(f"Status Check: \n Humidity is {current_humidity}%. \n No action required.")
 
-    # this actually displays all the data on the webpage in my specific listed format as allowed from the sort_keys line above
+    # this actually returns the JSON data that your dashboard's JavaScript will use
     return jsonify({
         "humidity" : current_humidity,
         "location" : "Grain Silo North",
         "humidistat_sensor_status" : "Active"
     })
 
-# this makes it so you need to start app.py directly and not if it is imported somewhere else in a different project module. 
+# ==========================================================================
+# This is the correct startup logic for Cloud Run
+# ==========================================================================
 if __name__ == '__main__':
     # Get the port number from the environment variable PORT
     # Default to 5001 if the environment variable is not set (for local testing)
@@ -79,3 +81,4 @@ if __name__ == '__main__':
     
     # Run the app, listening on all interfaces and on the correct port
     app.run(debug=True, host='0.0.0.0', port=port)
+
